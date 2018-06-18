e73382adc19219f9157c83c65c5160f4ea94c2d8
sarumathi1894 <sarumathi.s@vvdntech.in>
2018-06-18 19:48:53 +0530

    As per the comments proper space is added

--- oct_to_hex.py
+++ oct_to_hex.py
@@ -1,9 +1,9 @@
-a=input("enter the value:")
+a = input("enter the value:")
 b = int(a)
 print "Interger: ",b
-c=oct(b)
+c = oct(b)
 print "Conversion of Integer to octet is:",c
-d=eval(c)
+d = eval(c)
 print "evaluate of octet:",d
-e=hex(d)
+e = hex(d)
 print "conversion of octet to hexadecimal",e
