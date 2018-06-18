3a67d96562772f06011f5dfa524864978422bbd7
sarumathi1894 <sarumathi.s@vvdntech.in>
2018-06-18 10:52:16 +0530

    search a num in list/dict

---
+++ search.py
@@ -0,0 +1,17 @@
+#List
+
+#Dictionary
+d= {'a':'saru','b':'indu'}
+e=d.values()
+print "dict is:",d
+print "the  value present in dict",e
+f=d.keys()
+print "the key present in dictionary",f
+g=e.count('saru')
+print "the search string count is ",g
+l=[1,2,3,4,5,2,3]
+b=l.count(1)
+print "the list is",l
+print "the search string count is",b
+
+
