aba2f02839eb90e5bb9e26897a59a84b26f2a93b
sarumathi1894 <sarumathi.s@vvdntech.in>
2018-06-18 10:51:30 +0530

    oct to hex conversion

---
+++ oct_to_hex.py
@@ -0,0 +1,9 @@
+a=input("enter the value:")
+b = int(a)
+print "Interger: ",b
+c=oct(b)
+print "Conversion of Integer to octet is:",c
+d=eval(c)
+print "evaluate of octet:",d
+e=hex(d)
+print "conversion of octet to hexadecimal",e
